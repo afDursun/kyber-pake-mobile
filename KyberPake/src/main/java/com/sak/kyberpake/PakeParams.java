d04928eadd7adaf8af34331c0980b61c603f4a02
afDursun <faruk.dursun@bil.omu.edu.tr>
2022-11-18 18:32:07 +0300

    KyberPake

--- KyberPake/src/main/java/com/sak/kyberpake/PakeParams.java
+++ KyberPake/src/main/java/com/sak/kyberpake/PakeParams.java
@@ -8,11 +8,9 @@ public class PakeParams {
     public static int PAKE_KEYBYTES = 32;
     public static int MLWE_D = 2;
     public static int POLY_BYTES = 416 ;
-
-    public static int POLYVEC_BYTES = (MLWE_D * POLY_BYTES);
     public static int HASH_BYTES =  (2*ID_BYTES ) + (3 * (4* KyberParams.KYBER_POLYBYTES)) + PAKE_KEYBYTES ;
 
 
-    public static int PAKE_SENDC0 = ID_BYTES + 768 + SEED_BYTES ;
+    public static int PAKE_SENDC0 = ID_BYTES + KyberParams.KYBER_POLYBYTES512 + SEED_BYTES ;
 
 }
